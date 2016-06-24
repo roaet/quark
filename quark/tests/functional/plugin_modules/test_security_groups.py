e9682b87e8689a9a77c80cd0d43560dd9df77cab
Justin Hammond <justin.hammond@rackspace.com>
2016-06-24 12:52:18 -0500

    Work on making worker plugins
    JIRA:NCP-1172
    
    Fixed redis-base to use configurable min_sentinels
    
    Must enable environment variable for this to work
    
    SG Update workflow:
    
    - PUT to /security-groups
        - Parent task is made and sent to producer RPC
             - Creates the task id and then returns it to the main call
             - Then starts making sub tasks for each port associated with the SG
                - Each sub task will perform the required redis calls and update
                  its provided subtask to completed (or error)
    - Returns normal SG body + task information
    
    Updated the workflow to use the rule_id as the resource_id for the TX
    
    Transactions now autocomplete after updating their subtransactions
    
    Conflicts:
    	quark/db/migration/alembic/versions/HEAD
    
    Change-Id: I44545f2edb410d19e3156ee934dca283857c10d9

---
+++ quark/tests/functional/plugin_modules/test_security_groups.py
@@ -0,0 +1,34 @@
+# Copyright 2016 Rackspace Hosting Inc.
+# All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for# the specific language governing permissions and limitations
+#  under the License.
+
+import mock
+import netaddr
+
+import contextlib
+
+from neutron.common import exceptions as q_exc
+
+from quark.db import api as db_api
+import quark.plugin_modules.mac_address_ranges as macrng_api
+import quark.plugin_modules.networks as network_api
+import quark.plugin_modules.ports as port_api
+import quark.plugin_modules.subnets as subnet_api
+from quark.tests.functional.base import BaseFunctionalTest
+
+
+class SecurityGroupsAsyncUpdateTests(BaseFunctionalTest):
+
+    def test_sg_gather_associated_ports(self):
+        pass
